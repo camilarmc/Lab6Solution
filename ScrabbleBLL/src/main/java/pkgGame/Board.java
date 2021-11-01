70cef0cb0e0e30a12d3c7494b11aaf58cc3d2186
exmma <exmma@128.4.123.214>
2021-10-31 23:10:20 -0400

    bottom 3 rows of bonuses

--- ScrabbleBLL/src/main/java/pkgGame/Board.java
+++ ScrabbleBLL/src/main/java/pkgGame/Board.java
@@ -59,6 +59,31 @@ public class Board {
 	 */
 	private void SetBonus() {
 		// TODO: Set the bonus squares. Use the default / class Scrabble settings.
+		this.puzzle[14][0] = new Space(new BonusSquare(14,0, eBonusType.TripleWord));
+		this.puzzle[14][3] = new Space(new BonusSquare(14,3, eBonusType.DoubleLetter));
+		this.puzzle[14][7] = new Space(new BonusSquare(14,7, eBonusType.TripleWord));
+		this.puzzle[14][11] = new Space(new BonusSquare(14,11, eBonusType.DoubleLetter));
+		this.puzzle[14][14] = new Space(new BonusSquare(14,14, eBonusType.TripleWord));
+		this.puzzle[13][1] = new Space(new BonusSquare(13,1, eBonusType.DoubleWord));
+		this.puzzle[13][5] = new Space(new BonusSquare(13,5, eBonusType.TripleLetter));
+		this.puzzle[13][9] = new Space(new BonusSquare(13,9, eBonusType.TripleLetter));
+		this.puzzle[13][13] = new Space(new BonusSquare(13,13, eBonusType.DoubleWord));
+		this.puzzle[12][2] = new Space(new BonusSquare(12,2, eBonusType.DoubleWord));
+		this.puzzle[12][6] = new Space(new BonusSquare(12,6, eBonusType.DoubleLetter));
+		this.puzzle[12][8] = new Space(new BonusSquare(12,8, eBonusType.DoubleLetter));
+		this.puzzle[12][12] = new Space(new BonusSquare(12,12, eBonusType.DoubleWord));
+
+
+
+
+
+
+
+
+
+
+
+
 	}
 
 }
